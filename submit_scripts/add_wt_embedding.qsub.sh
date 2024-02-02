14d57232857faa4ee8d23c49181a163a10393fbe
Jude Wells <judewells@hotmail.com>
2024-02-02 22:01:33 +0000

    add wt embedding submit script

---
+++ submit_scripts/add_wt_embedding.qsub.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#$ -l tmem=32G
+#$ -l h_vmem=32G
+#$ -l h_rt=82:0:0
+# -l gpu=false
+#$ -S /bin/bash
+#$ -N embed_test
+#$ -wd /SAN/orengolab/nsp13/protein_gym/make_esm_embeddings
+# -t 1 # subjobs for ${SGE_TASK_ID}
+#$ -o /SAN/orengolab/nsp13/protein_gym/qsub_logs/
+#$ -j y
+hostname
+date
+source /share/apps/source_files/python/python-3.9.5.source
+source /SAN/orengolab/nsp13/esm_env/bin/activate
+cd /SAN/orengolab/nsp13/protein_gym/make_esm_embeddings
+export TORCH_HOME=/SAN/orengolab/nsp13/esm_env/torch_home
+python3 add_wt_embedding.py
+date
